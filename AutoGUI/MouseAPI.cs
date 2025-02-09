be85db11f01a2ac8e4ddb9588c6f1baa02755cc5
mamawhes <1609523608@qq.com>
2025-02-09 15:34:20 +0800

    完成了Mac的鼠标移动和点击功能

--- AutoGUI/MouseAPI.cs
+++ AutoGUI/MouseAPI.cs
@@ -10,29 +10,29 @@ namespace AutoGUI
         Right = 1,
         Middle = 2,
     }
-    public interface IMouseAPI
+    public class MouseAPI
     {
-        public static void MouseMove(int x, int y)
+        public virtual void MouseMove(int x, int y)
         {
             Console.WriteLine($"mouse move to ({x},{y})");
         }
 
-        public static void MouseDown(MouseButton mouseButton)
+        public virtual void MouseDown(MouseButton mouseButton)
         {
             Console.WriteLine($"mouse down to ({(int)mouseButton})");
         }
-        public static void MouseUp(MouseButton mouseButton)
+        public virtual void MouseUp(MouseButton mouseButton)
         {
             Console.WriteLine($"mouse up to ({(int)mouseButton})");
         }
 
-        public static void MouseClick(MouseButton mouseButton)
+        public void MouseClick(MouseButton mouseButton)
         {
             MouseDown(mouseButton); 
             MouseUp(mouseButton);
         }
 
-        public static void MouseDoubleClick(MouseButton mouseButton=MouseButton.Left)
+        public void MouseDoubleClick(MouseButton mouseButton=MouseButton.Left)
         {
             MouseClick(mouseButton);
             System.Threading.Thread.Sleep(100);
@@ -40,34 +40,41 @@ namespace AutoGUI
         }
     }
 
-    public class MouseAPIForMac: IMouseAPI
+    public class MouseAPIForMac: MouseAPI
     {
-        // 导入 Core Graphics 函数
-        [DllImport("/System/Library/Frameworks/CoreGraphics.framework/CoreGraphics")]
-        private static extern IntPtr CGEventCreateMouseEvent(IntPtr source, uint mouseType, CGPoint point, uint mouseButton);
-
-        [DllImport("/System/Library/Frameworks/CoreGraphics.framework/CoreGraphics")]
-        private static extern void CGEventPost(uint tap, IntPtr eventRef);
-
-        [DllImport("/System/Library/Frameworks/CoreGraphics.framework/CoreGraphics")]
-        private static extern void CFRelease(IntPtr obj);
-
-        [DllImport("/System/Library/Frameworks/CoreGraphics.framework/CoreGraphics")]
-        private static extern void CGEventSetIntegerValueField(IntPtr eventRef, uint field, long value);
+        // 导入 macOS 的 Core Graphics 框架
+        private const string CoreGraphicsLibrary = "/System/Library/Frameworks/CoreGraphics.framework/CoreGraphics";
 
         // 定义鼠标事件类型
-        private const uint kCGEventMouseMoved = 5;
-        private const uint kCGEventLeftMouseDown = 1;
-        private const uint kCGEventLeftMouseUp = 2;
-        private const uint kCGEventRightMouseDown = 3;
-        private const uint kCGEventRightMouseUp = 4;
-        private const uint kCGEventOtherMouseDown = 25;
-        private const uint kCGEventOtherMouseUp = 26;
-        private const uint kCGEventScrollWheel = 22;
-        private const uint kCGHIDEventTap = 0;
+        private enum CGEventType
+        {
+            MouseMoved = 5,
+            LeftMouseDown = 1,
+            LeftMouseUp = 2,
+            RightMouseDown = 3,
+            RightMouseUp = 4,
+            MiddleMouseDown = 25,
+            MiddleMouseUp = 26,
+            ScrollWheel = 22
+        }
+        // 定义鼠标滚轮事件类型
+        private enum CGScrollEventUnit
+        {
+            Line = 0,
+            Pixel = 1
+        }
+        // 导入 Core Graphics 函数
+        [DllImport(CoreGraphicsLibrary)]
+        private static extern IntPtr CGEventCreateMouseEvent(IntPtr source, CGEventType mouseType, CGPoint position, MouseButton button);
 
-        // 定义鼠标滚轮字段
-        private const uint kCGScrollWheelEventDeltaAxis1 = 11;
+        [DllImport(CoreGraphicsLibrary)]
+        private static extern void CGEventPost(int tap, IntPtr evt);
+
+        [DllImport(CoreGraphicsLibrary)]
+        private static extern void CFRelease(IntPtr obj);
+
+        [DllImport(CoreGraphicsLibrary)]
+        private static extern void CGEventSetIntegerValueField(IntPtr evt, int field, long value);
 
         // 定义 CGPoint 结构
         [StructLayout(LayoutKind.Sequential)]
@@ -82,31 +89,53 @@ namespace AutoGUI
                 Y = y;
             }
         }
-
-        public static void MouseMove(int x, int y)
+        private static CGPoint _pos=new CGPoint(0,0);
+        public override void MouseMove(int x, int y)
         {
-            IMouseAPI.MouseMove(x, y);
+            _pos = new CGPoint(x, y);
+            IntPtr eventRef = CGEventCreateMouseEvent(IntPtr.Zero, CGEventType.MouseMoved, _pos, MouseButton.Left);
+            CGEventPost(0, eventRef);
+            CFRelease(eventRef);
         }
 
-        public static void MouseDown(MouseButton mouseButton)
+        public override void MouseDown(MouseButton mouseButton)
         {
-            IMouseAPI.MouseDown(mouseButton);
+
+            CGEventType eventType=CGEventType.LeftMouseDown;
+            if (mouseButton == MouseButton.Left)
+            {
+                eventType = CGEventType.LeftMouseDown;
+            }else if (mouseButton == MouseButton.Right)
+            {
+                eventType = CGEventType.RightMouseDown;
+            }else if (mouseButton == MouseButton.Middle)
+            {
+                eventType = CGEventType.MiddleMouseDown;
+            }
+            IntPtr eventRef = CGEventCreateMouseEvent(IntPtr.Zero, eventType, _pos, mouseButton);
+            CGEventPost(0, eventRef);
+            CFRelease(eventRef);
         }
 
-        public static void MouseUp(MouseButton mouseButton)
+        public override void MouseUp(MouseButton mouseButton)
         {
-            IMouseAPI.MouseUp(mouseButton);
+            CGEventType eventType=CGEventType.LeftMouseUp;
+            if (mouseButton == MouseButton.Left)
+            {
+                eventType = CGEventType.LeftMouseUp;
+            }else if (mouseButton == MouseButton.Right)
+            {
+                eventType = CGEventType.RightMouseUp;
+            }else if (mouseButton == MouseButton.Middle)
+            {
+                eventType = CGEventType.MiddleMouseUp;
+            }
+            IntPtr eventRef = CGEventCreateMouseEvent(IntPtr.Zero, eventType, _pos, mouseButton);
+            CGEventPost(0, eventRef);
+            CFRelease(eventRef);
         }
 
-        public static void MouseClick(MouseButton mouseButton)
-        {
-            IMouseAPI.MouseClick(mouseButton);
-        }
-
-        public static void MouseDoubleClick(MouseButton mouseButton)
-        {
-            IMouseAPI.MouseDoubleClick(mouseButton);
-        }
+  
         
 
     }
--- AutoGUI/MouseController.cs
+++ AutoGUI/MouseController.cs
@@ -1,6 +1,72 @@
-namespace AutoGUI;
+using System.Numerics;
+using System.Runtime.InteropServices;
 
-public class MouseController
+namespace AutoGUI
 {
     
+    public static class MouseController
+    {
+        private static MouseAPI _api ;
+
+        private static void _Init()
+        {
+            // ReSharper disable once ConditionIsAlwaysTrueOrFalseAccordingToNullableAPIContract
+            if (_api == null)
+            {
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    _api = new MouseAPI();
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                {
+                    _api = new MouseAPIForMac();
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                {
+                    _api = new MouseAPI();
+                }
+                else
+                {
+                    _api = new MouseAPI();
+                }
+            }
+        }
+
+        public static void MouseDown(MouseButton mouseButton)
+        {
+            _Init();
+            _api.MouseDown(mouseButton);
+        }
+
+        public static void MouseMove(int x, int y)
+        {
+            _Init();
+            _api.MouseMove(x,y);
+        }
+
+        public static void MouseUp(MouseButton mouseButton)
+        {
+            _Init();
+            _api.MouseUp(mouseButton);
+        }
+
+        public static void MouseWheel(int delta)
+        {
+            _Init();
+            
+        }
+
+        public static void MouseClick(MouseButton mouseButton)
+        {
+            _Init();
+            _api.MouseClick(mouseButton);
+        }
+
+        public static void MouseDoubleClick(MouseButton mouseButton)
+        {
+            _Init();
+            _api.MouseDoubleClick(mouseButton);
+        }
+    }
 }
+
