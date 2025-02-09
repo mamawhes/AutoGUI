cf1982af45056d09bd99576548c3b0992dc5fb48
mamawhes <1609523608@qq.com>
2025-02-09 15:39:44 +0800

    测试脚本

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp;
+using AutoGUI;
+class Program
+{
+    static void Main(string[] args)
+    {
+        MouseController.MouseMove(100,10);
+       
+        MouseController.MouseClick(MouseButton.Left);
+        System.Threading.Thread.Sleep(1000);
+        MouseController.MouseMove(100,50);
+        MouseController.MouseClick(MouseButton.Left);
+        //MouseController.MouseDoubleClick(MouseButton.Left);
+        
+        Console.WriteLine("Hello, World!");
+    }
+}
